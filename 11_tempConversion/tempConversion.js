69213c757a2ef373e6d7576fdf759e6c2682bab1
Jaden Shelton <jaden@sheltonz.co>
2025-12-04 16:15:16 -0700

    Complete exercise 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+   let newTemp = ((temperature - 32) * (5 / 9));
+   if(Number.isInteger(newTemp)){
+    return newTemp;
+   } else {
+    return Number(newTemp.toFixed(1));
+   }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let newTemp = (temperature * (9 / 5) + 32);
+  if(Number.isInteger(newTemp)){
+    return newTemp;
+  } else {
+    return Number(newTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
